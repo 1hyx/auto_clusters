f9ee551d2ac16ae434b536ba9816c34f35a5ecf5
rene0410 <hyx552211@163.com>
2019-10-11 17:49:05 +0800

    use class with demo

--- class_study/product.py
+++ class_study/product.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 
+# 生成的accounts 和 价格构成了 完整的产品信息表
 from class_study.generate_account import AccountGenerator
 from class_study.generate_product_list import ProductPrice
 
@@ -14,19 +15,34 @@ class ProductList:
     def __init__(self, quantity):
         self.quantity = quantity
 
+    """
+    set_id 函数可以保证生成ID的格式
+    length 自定义部分长度
+    structure [{'type':'char','num':4},{'type':'num', 'num':4}] 要求设置的随机生成部分的结构
+    prefix 前缀
+    suffix 后缀
+    """
     def set_id(self, length, structure=[], prefix='', suffix=''):
         self.id_length = length
         self.id_structure = structure
         self.id_prefix = prefix
         self.id_suffix = suffix
 
-    max_price = 500
     min_price = 3
+    max_price = min_price+500
 
+    """
+    set_price 函数保证设计的价格的最大值和最小值
+    min_price: 最低价格
+    max_price: 最高价格
+    """
     def set_price(self, max_price, min_price):
         self.max_price = max_price
         self.min_price = min_price
 
+    """
+    最终获得商品价格编号列表的函数
+    """
     def get_product_list(self):
         acc_generator = AccountGenerator()
         acc = acc_generator.generate_account(quantity=self.quantity, length=self.id_length, prefix=self.id_prefix,
